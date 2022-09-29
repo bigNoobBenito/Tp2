43b1fcf0a2d932b828cee5d6fdf8fa093fb6eaec
bigNoobBenito <kyehouenou@gmail.com>
2022-09-29 10:23:41 -0400

    j'ai rajoute une fonction qui demande la porte des nombres que l'utilisateur peut choisir J'ai mis des commentaires dans mon code

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+"""
+Ce code a été fait par Benito Karol Yehouenou le 29 Septembre 2022
+Groupe:
+Ce code est un jeu de devinette qui demande a l'utilisateur de deviner le nombre que l'ordinateur a choisit
+C'est l'utilisateur qui decide jusqu'ou est ce que l'ordinateur peut choisir ses nombres
+"""
+
 import random
+
+
+def choosing_number():
+    """
+    Cette fonction demande a l'utilisateur le nombre qu'il peut choisir parmi une range de nombre
+    :return: le nombre aléatoire choisis
+    """
+    range1 = int(input("vous devez choisir entre deux nombres, choisissez le premier"))
+    range2 = int(input("vous devez choisir entre deux nombres, choisissez le second"))
+    return random.randint(range1, range2)
+
 
 playing = True
 while playing:
-    range1 =int(input("vous devez choisir entre deux nombres, choisissez le premier"))
-    range2 =int(input("vous devez choisir entre deux nombres, choisissez le second"))
-    pc_number = random.randint(range1,range2)
+    number_range = choosing_number()
     winning = False
     number_try = 0
     while not winning:
-
         number_try += 1
 
-        try:
-            gess = int(input(" choisissez un nombre entre 1 et 1000"))
-        except ValueError:
-            print(" vous devez choisir un nombre seulement! ")
+        gess_numbers = int(input(" choisissez un nombre entre les nombres que vous avez chosis"))
 
-        if gess > pc_number:
+        if gess_numbers > number_range:
             print("votre nombre est trop grand, choisissez un plus petit")
-        elif gess < pc_number:
+        elif gess_numbers < number_range:
             print("votre nombre est trop petit, choisissez un plus grand")
         else:
             print("Vous avez fini en:", number_try)
@@ -27,5 +39,3 @@ while playing:
     recommencer = input("est ce que vous voulez recommencez y/n")
     if recommencer == "n":
         playing = False
-
-
